f649ff2deba20c343aee43234ed29eb08c459a4c
Bekzat Kamen <begzat.kamen@gmail.com>
2025-12-23 19:59:56 +0500

    Add task.go and local_storage.go

--- internal/entity/task.go
+++ internal/entity/task.go
@@ -1,1 +1,8 @@
 package entity
+
+type Task struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"desc"`
+	IsDone      bool   `json:"done"`
+}
--- internal/repository/local_storage.go
+++ internal/repository/local_storage.go
@@ -1,1 +1,27 @@
 package repository
+
+import "todo-app/internal/entity"
+
+type TaskRepository interface {
+	Create(task *entity.Task) error
+	GetAll() ([]*entity.Task, error)
+}
+
+type LocalStorage struct {
+	db []*entity.Task
+}
+
+func NewLocalStorage() *LocalStorage {
+	return &LocalStorage{
+		db: make([]*entity.Task, 0),
+	}
+}
+
+func (l *LocalStorage) Create(task *entity.Task) error {
+	l.db = append(l.db, task)
+	return nil
+}
+
+func (l *LocalStorage) GetAll() ([]*entity.Task, error) {
+	return l.db, nil
+}
